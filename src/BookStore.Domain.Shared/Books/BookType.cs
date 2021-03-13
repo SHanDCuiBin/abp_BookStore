684d91e9b045f22a89a730af97f3163a7525f02b
cuibin <sdcuibin@outlook.com>
2021-03-13 15:34:30 +0800

    添加项目文件。

---
+++ src/BookStore.Domain.Shared/Books/BookType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Books
+{
+    public enum BookType
+    {
+        Undefined,        //未分类
+        Adventrue,        //冒险
+        Biography,        //传记
+        Dystopia,         //
+        Fantastic,        //
+        Horror,           //恐怖
+        Science,          //科学
+        ScienceFiction,   //科幻小说
+        Poetry            //诗歌
+    }
+}
